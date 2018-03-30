ddad6cd93df8999bb9045901f59062748d692808
Rhon Joshua Calupitan <35456686+rjcalupitan@users.noreply.github.com>
2018-03-30 17:21:19 +0800

    Add files via upload

---
+++ File_Handling.py
@@ -0,0 +1,8 @@
+text_file = open("text.txt",'w')
+text_file.write("Hello! This is a Python Program.")
+text_file.close()
+text_file = open("text.txt",'a')
+text_file.write(" asdfghjkl")
+text_file.close()
+
+
